31f14649bb3faf60ad4e807b310d756043bd049a
Fangyuwenwen <35857338+Fangyuwenwen@users.noreply.github.com>
2022-11-29 18:51:46 +0800

    11_29_1

--- thing_1.py
+++ thing_1.py
@@ -1,18 +1,36 @@
-import urequests
+from urllib.request import urlopen
+
+import json
 import time
 
-host='http://api.thingspeak.com'
-read_api_key='1KD7G3JFAE0L6Q4O'
-channel_id='1680573'
 
-url='%s/channels/%s/feeds/last.json?api_key=%s' \
-     %(host, channel_id, read_api_key)
+READ_API_KEY='O0TENR74YMQ8ORIT'
+CHANNEL_ID='1886703'
+
 
 while True:
-    try:
-        r=urequests.get(url)
-        print(r.text)
-        print(r.json())
-    except:
-        print('urequests.get() exception occurred!')
-    time.sleep(3)
+    TS = urlopen("https://api.thingspeak.com/channels/1886703/fields/1.json?api_key=O0TENR74YMQ8ORIT&results=2")
+
+    response = TS.read()
+
+    data=json.loads(response.decode('utf-8'))
+
+    print(data)
+
+    print (data["feeds"][1]["field1"])
+
+    a=data["feeds"][1]["field1"]
+
+    if a=='1' :
+        print("wood")
+        break
+
+    elif a=='2' :
+        print("metal")
+        break
+
+    else:
+        print("plastic")
+        break
+
+TS.close()
