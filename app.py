017bde566aada9ebbf6155144bb0064a9ede0100
Fangyuwenwen <35857338+Fangyuwenwen@users.noreply.github.com>
2023-02-12 15:25:40 +0800

    02_12

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def handle_message(event):
     elif message_text == '雷達':
         line_bot_api.reply_image(
             event.reply_token,
-            ImageMessage('雷達',reply_image.req)
+            ImageSendMessage('雷達',reply_image.req)
         )
     else:
         line_bot_api.reply_message(
