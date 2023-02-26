55eb4d3c8f57977cc10b3c62f04be5fff579b68d
Fangyuwenwen <35857338+Fangyuwenwen@users.noreply.github.com>
2023-02-26 13:20:57 +0000

    02_26_2

--- app.py
+++ app.py
@@ -64,6 +64,20 @@ data=json.loads(response.decode('utf-8'))
 tem_value=str(data["channel"]["field1"]+data["feeds"][1]["field1"])
 hum_value=str(data["channel"]["field2"]+data["feeds"][1]["field2"])
 
+#取得天氣資料
+cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園縣','高雄市','新竹市','屏東縣','新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
+
+def get(city):
+    token = 'CWB-F99C4A80-6BBC-4597-8238-CD2DF9C871E8'
+    url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=' + token + '&format=JSON&locationName=' + str(city)
+    Data = requests.get(url)
+    Data = (json.loads(Data.text,encoding='utf-8'))['records']['location'][0]['weatherElement']
+    res = [[] , [] , []]
+    for j in range(3):
+        for i in Data:
+            res[j].append(i['time'][j])
+    return res
+
 # function for create tmp dir for download content
 def make_static_tmp_dir():
     try:
@@ -96,20 +110,6 @@ def callback():
         abort(400)
 
     return 'OK'
-
-#天氣
-cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園縣','高雄市','新竹市','屏東縣','新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
-
-def get(city):
-    token = 'CWB-F99C4A80-6BBC-4597-8238-CD2DF9C871E8'
-    url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=' + token + '&format=JSON&locationName=' + str(city)
-    Data = requests.get(url)
-    Data = (json.loads(Data.text,encoding='utf-8'))['records']['location'][0]['weatherElement']
-    res = [[] , [] , []]
-    for j in range(3):
-        for i in Data:
-            res[j].append(i['time'][j])
-    return res
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
