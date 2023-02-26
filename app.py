e5be93bbfc0d1ffb9b19a28266dbb39fe08d864d
Fangyuwenwen <35857338+Fangyuwenwen@users.noreply.github.com>
2023-02-26 07:35:47 +0000

    02_26

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ from linebot.models import (
 
 #arduino
 from urllib.request import urlopen
-import json
+import json,requests
 import time
 
 #thingspeak
@@ -97,6 +97,19 @@ def callback():
 
     return 'OK'
 
+#天氣
+cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園縣','高雄市','新竹市','屏東縣','新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
+
+def get(city):
+    token = 'CWB-F99C4A80-6BBC-4597-8238-CD2DF9C871E8'
+    url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=' + token + '&format=JSON&locationName=' + str(city)
+    Data = requests.get(url)
+    Data = (json.loads(Data.text,encoding='utf-8'))['records']['location'][0]['weatherElement']
+    res = [[] , [] , []]
+    for j in range(3):
+        for i in Data:
+            res[j].append(i['time'][j])
+    return res
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -115,6 +128,34 @@ def handle_message(event):
             event.reply_token,
             FlexSendMessage('圖表',flexmessage)
         )
+    elif message_text == '天氣':
+        city = message_text[3:]
+        city = city.replace('台','臺')
+        if(not (city in cities)):
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="查詢格式為: 天氣 縣市"))
+        else:
+            res = get(city)
+            line_bot_api.reply_message(
+                event.reply_token, TemplateSendMessage(
+                alt_text = city + '未來 36 小時天氣預測',
+                template = CarouselTemplate(
+                    columns = [
+                        CarouselColumn(
+                            thumbnail_image_url = 'https://i.imgur.com/Ex3Opfo.png',
+                            title = '{} ~ {}'.format(res[0][0]['startTime'][5:-3],res[0][0]['endTime'][5:-3]),
+                            text = '天氣狀況 {}\n溫度 {} ~ {} °C\n降雨機率 {}'.format(data[0]['parameter']['parameterName'],data[2]['parameter']['parameterName'],data[4]['parameter']['parameterName'],data[1]['parameter']['parameterName']),
+                            actions = [
+                                URIAction(
+                                    label = '詳細內容',
+                                    uri = 'https://www.cwb.gov.tw/V8/C/W/County/index.html'
+                                )
+                            ]
+                        )for data in res
+                    ]
+                )
+            ))
     else:
         line_bot_api.reply_message(
             event.reply_token,
