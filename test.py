98d44f0f38892ecda13580f2a9edc5a29d338812
Fangyuwenwen <35857338+Fangyuwenwen@users.noreply.github.com>
2023-02-28 07:35:56 +0000

    02_28_2

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ token = 'CWB-F99C4A80-6BBC-4597-8238-CD2DF9C871E8'
 url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=' + token + '&format=JSON&locationName=雲林縣'
 Data = requests.get(url)
 Data = (json.loads(Data.text))['records']['location'][0]['weatherElement']
-#print(Data)
+print(Data)
 res = [[] , [] , []]
 for j in range(3):
         for i in Data:
             res[j].append(i['time'][j])
-print(res)
+#print(res)
