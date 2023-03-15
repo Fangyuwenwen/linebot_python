d651d9b970f237d71f1a5e15bd7a53d9f88548cd
Fangyuwenwen <35857338+Fangyuwenwen@users.noreply.github.com>
2023-03-15 02:50:40 +0000

    03_15_4

--- app.py
+++ app.py
@@ -117,7 +117,26 @@ def city_status(city):
     except:
         return msg    # 如果取資料有發生錯誤，直接回傳 msg
     
-#取得雷達回波圖
+#取得地震資訊
+def earth_quake():
+    msg = ['找不到地震資訊','找不到地震資訊']            # 預設回傳的訊息
+    try:
+        code = 'CWB-F99C4A80-6BBC-4597-8238-CD2DF9C871E8'
+        url = f'https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={code}'
+        e_data = requests.get(url)                                   # 爬取地震資訊網址
+        e_data_json = e_data.json()                                  # json 格式化訊息內容
+        eq = e_data_json['records']['earthquake']                    # 取出地震資訊
+        for i in eq:
+            loc = i['earthquakeInfo']['epiCenter']['location']       # 地震地點
+            val = i['earthquakeInfo']['magnitude']['magnitudeValue'] # 地震規模
+            dep = i['earthquakeInfo']['depth']['value']              # 地震深度
+            eq_time = i['earthquakeInfo']['originTime']              # 地震時間
+            img = i['reportImageURI']                                # 地震圖
+            msg = [f'{loc}，芮氏規模 {val} 級，深度 {dep} 公里，發生時間 {eq_time}。', img]
+            break     # 取出第一筆資料後就 break
+        return msg    # 回傳 msg
+    except:
+        return msg    # 如果取資料有發生錯誤，直接回傳 msg
 
 
 # function for create tmp dir for download content
@@ -231,6 +250,11 @@ def handle_message(event):
                 event.reply_token,
                 ImageSendMessage(original_content_url='https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-A0058-003.png?', preview_image_url='https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-A0058-003.png?')
                 )
+    elif message_text == '地震':
+        msg=earth_quake()
+        line_bot_api.reply_message(
+            event.reply_token, 
+            TextSendMessage(text=msg))
     else:
         line_bot_api.reply_message(
             event.reply_token,
