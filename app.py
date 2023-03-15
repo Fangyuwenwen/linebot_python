92bd8dac2ccbbd844dd75386bce8e9961815ae48
Fangyuwenwen <35857338+Fangyuwenwen@users.noreply.github.com>
2023-03-15 03:09:51 +0000

    03_15_5

--- app.py
+++ app.py
@@ -254,7 +254,6 @@ def handle_message(event):
         msg=earth_quake()
         line_bot_api.reply_message(
             event.reply_token, 
-            TextSendMessage(text=msg[0]),
             ImageSendMessage(original_content_url=msg[1],preview_image_url=[1])
             )
     else:
