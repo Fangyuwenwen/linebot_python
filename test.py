742a73286e4e8d3ef37e2fa9461370737af828a3
Fangyuwenwen <35857338+Fangyuwenwen@users.noreply.github.com>
2023-03-18 02:02:50 +0000

    03_18

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-"""  import json,requests
+import json,requests
 cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園縣','高雄市','新竹市','屏東縣','新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
 print(cities)
 
 token = 'CWB-F99C4A80-6BBC-4597-8238-CD2DF9C871E8'
 url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=' + token + '&format=JSON&locationName=雲林縣'
+#https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-F99C4A80-6BBC-4597-8238-CD2DF9C871E8&format=JSON&locationName=雲林縣
 Data = requests.get(url)
 Data = (json.loads(Data.text))['records']['location'][0]['weatherElement']
 print(Data)
@@ -50,7 +51,7 @@ for i in site_list:
         msg = f'空氣品質{site_list[i]["status"]} ( AQI {site_list[i]["aqi"]} )。'
         break"""
 
-import requests
+"""import requests
 def earth_quake():
     msg = ['找不到地震資訊','找不到地震資訊']            # 預設回傳的訊息
     code = 'CWB-F99C4A80-6BBC-4597-8238-CD2DF9C871E8'  #https://opendata.cwb.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization=CWB-F99C4A80-6BBC-4597-8238-CD2DF9C871E8
@@ -68,4 +69,4 @@ def earth_quake():
         break     # 取出第一筆資料後就 break
     return msg    # 回傳 msg
 a=earth_quake()
-print(a[0])
+print(a[0])"""
