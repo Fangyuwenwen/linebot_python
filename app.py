7581f1326b2832def8e60533e57e5a87e606fd94
Fangyuwenwen <35857338+Fangyuwenwen@users.noreply.github.com>
2023-03-20 08:18:26 +0000

    03_20_3

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ import json
 import os
 import sys
 import tempfile
-import news.py
+import requests 
+from bs4 import BeautifulSoup
+import pandas as pd
+
 from urllib.request import urlopen
 from argparse import ArgumentParser
 
@@ -135,6 +138,25 @@ def earth_quake():
         msg = [f'{loc}，芮氏規模 {val} 級，深度 {dep} 公里，發生時間 {eq_time}。', img]
         break     # 取出第一筆資料後就 break
     return msg    # 回傳 msg
+
+#爬取最新新聞
+def news(): 
+    url = 'https://news.google.com/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JYcG9MVlJYR2dKVVZ5Z0FQAQ?hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
+    r = requests.get(url)
+    web_content = r.text
+    soup = BeautifulSoup(web_content,'lxml')
+    title = soup.find_all('div', class_='XlKvRb',limit=5)
+    #print(title)
+    titles = [t.find('a')['aria-label'] for t in title]
+    #print(titles)
+    newUrls = [requests.get(t.find('a')['href'].replace('.','https://news.google.com',1)).url for t in title]
+    #print(newUrls)
+    df = pd.DataFrame(
+    {
+        'title': titles,
+        'links': newUrls
+    })
+    return df
 
 # function for create tmp dir for download content
 def make_static_tmp_dir():
@@ -300,7 +322,5 @@ if __name__ == "__main__":
 
     # create tmp dir for download content
     make_static_tmp_dir()
-
-    news.news()
     
     app.run(debug=options.debug, port=options.port)
