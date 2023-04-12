8c54fd4e3e0facd5671701cf706d5775f0432a88
Fangyuwenwen <a0989586253@gmail.com>
2023-04-12 14:54:00 +0800

    04_12_new

--- menu.py
+++ menu.py
@@ -5,22 +5,26 @@ import json
 headers = {'Authorization':'Bearer 4kW19F7L5Yt+DKSvppThCuirviV8iyqGcEYrg8aM2NjaDNl4zyA5fsFebqJusjAEb5CLVAD/dC7eDl3m7E64ByD6qOoUB0h+jyFRZkfq5tZ+gBxdd/adTJri+vdiikYKn9J58RLux6L14oElp7BBRwdB04t89/1O/w1cDnyilFU=','Content-Type':'application/json'}
 
 body = {
-    'size': {'width': 2500, 'height': 640},    # 設定尺寸
+    'size': {'width': 2500, 'height': 843},    # 設定尺寸
     'selected': 'true',                        # 預設是否顯示
     'name': '選單',                             # 選單名稱
     'chatBarText': '功能選單',                        # 選單在 LINE 顯示的標題
     'areas':[                                  # 選單內容
         {
-          'bounds': {'x': 0, 'y': 0, 'width': 1250, 'height': 640},           # 選單位置與大小
-          'action': {'type': 'message', 'text':'請輸入天氣 +縣市'}  # 點擊後開啟地圖定位，傳送位置資訊
+          'bounds': {'x': 0, 'y': 0, 'width': 625, 'height': 843},           # 選單位置與大小
+          'action': {'type': 'message', 'text':'天氣'}  # 點擊後開啟地圖定位，傳送位置資訊
         },
         {
-          'bounds': {'x': 1251, 'y': 0, 'width':625, 'height': 640},     # 選單位置與大小
+          'bounds': {'x': 625, 'y': 0, 'width':625, 'height': 843},     # 選單位置與大小
           'action': {'type': 'message', 'text':'雷達'}               # 點擊後傳送文字
         },
         {
-          'bounds': {'x': 1879, 'y': 0, 'width':625, 'height': 640},     # 選單位置與大小
+          'bounds': {'x': 1250, 'y': 0, 'width':625, 'height': 843},     # 選單位置與大小
           'action': {'type': 'message', 'text':'地震'}               # 點擊後傳送文字
+        },
+        {
+          'bounds': {'x': 1875, 'y': 0, 'width': 625, 'height': 843},           # 選單位置與大小
+          'action': {'type': 'message', 'text':'空氣'}  # 點擊後開啟地圖定位，傳送位置資訊
         }
     ]
   }
@@ -36,14 +40,14 @@ print(req.text)"""
 
 line_bot_api = LineBotApi('4kW19F7L5Yt+DKSvppThCuirviV8iyqGcEYrg8aM2NjaDNl4zyA5fsFebqJusjAEb5CLVAD/dC7eDl3m7E64ByD6qOoUB0h+jyFRZkfq5tZ+gBxdd/adTJri+vdiikYKn9J58RLux6L14oElp7BBRwdB04t89/1O/w1cDnyilFU=')
 
-with open("richmenu.jpg", 'rb') as f:
-    line_bot_api.set_rich_menu_image("richmenu-cf19f8f01cdd3c25146194573bf28810", "image/jpeg", f)"""
+with open("menu.jpg", 'rb') as f:
+    line_bot_api.set_rich_menu_image("richmenu-f55bbd7524cd6ab120fa6519d17154e7", "image/jpeg", f)"""
     
 import requests
 
 headers = {"Authorization":"Bearer 4kW19F7L5Yt+DKSvppThCuirviV8iyqGcEYrg8aM2NjaDNl4zyA5fsFebqJusjAEb5CLVAD/dC7eDl3m7E64ByD6qOoUB0h+jyFRZkfq5tZ+gBxdd/adTJri+vdiikYKn9J58RLux6L14oElp7BBRwdB04t89/1O/w1cDnyilFU=","Content-Type":"application/json"}
 
-req = requests.request('POST', 'https://api.line.me/v2/bot/user/all/richmenu/richmenu-cf19f8f01cdd3c25146194573bf28810', 
+req = requests.request('POST', 'https://api.line.me/v2/bot/user/all/richmenu/richmenu-f55bbd7524cd6ab120fa6519d17154e7', 
                        headers=headers)
 
 print(req.text)
