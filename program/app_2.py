d1fb72a5ce8e0cd7ed1c71ddfe1970d105db8934
Fangyuwenwen <35857338+Fangyuwenwen@users.noreply.github.com>
2024-10-25 14:09:41 +0800

    Update app_2.py
    修改停車場程式

--- program/app_2.py
+++ program/app_2.py
@@ -645,7 +645,7 @@ def handle_message(event):
                     for j in i :
                         #mes_url.append("https://www.google.com.tw/maps/search/"+j['CarParkName'])
                         mes_url.append("https://www.google.com.tw/maps/search/")
-                        mes+=j['CarParkName']+"\n"
+                        #mes+=j['CarParkName']+"\n"
                     if len(mes) == 0:
                         line_bot_api.reply_message(
                             event.reply_token,
